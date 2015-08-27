02fd22afbb604c3882cbf2caa70ada8ed70e5a21
Sam Marschke <samuel.marschke@my.jcu.edu.au>
2015-08-27 14:43:38 +1000

    Finished GUI with adding People

--- SQLite Test java/src/com/company/Main.java
+++ SQLite Test java/src/com/company/Main.java
@@ -1,26 +1,63 @@
 package com.company;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
 import javax.swing.*;
-import java.util.Scanner;
 
 
-public class Main extends javax.swing.JFrame {
+
+public class Main {
     public static JFrame frame = new JFrame("SQL Test");
     public static JLabel label1 = new JLabel();
     public static JLabel label2 = new JLabel();
-    public static Scanner kb = new Scanner(System.in);
     public static JTextArea ta = new JTextArea();
     public static JButton butt = new JButton("Add Employee");
-    public static GroupLayout layout = new GroupLayout(frame.getContentPane());
+    public static Connection c = null;
     public static void main(String[] args) throws ClassNotFoundException  {
         Class.forName("org.sqlite.JDBC");
-        Connection c = null;
+
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
 
+        displayTable();
 
+
+        butt.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    c = DriverManager.getConnection("jdbc:sqlite:test.db");
+                    Statement statement = c.createStatement();
+                    statement.setQueryTimeout(30);
+
+                    statement.executeUpdate("insert into employees values(3, 'Brian')");
+
+                    c.close();
+
+                    displayTable();
+
+                } catch (Exception ex) {
+                    System.out.print(ex);
+                }
+            }
+        });
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(2, 2, 2, 2);
+        frame.getContentPane().add(ta, gbc);
+        gbc.gridy++;
+        frame.getContentPane().add(butt, gbc);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void displayTable() {
         try {
             c = DriverManager.getConnection("jdbc:sqlite:test.db");
             Statement statement = c.createStatement();
@@ -31,36 +68,25 @@ public class Main extends javax.swing.JFrame {
 //            statement.executeUpdate("insert into employees values(1, 'Josh')");
 //            statement.executeUpdate("insert into employees values(2, 'Ron')");
             ResultSet rs = statement.executeQuery("SELECT * FROM employees");
-//            frame.getContentPane().add(label1);
-//            frame.getContentPane().add(ta);
-//            frame.getContentPane().add(butt);
 
 
+            ta.setText("");
             while(rs.next()) {
                 System.out.println("--------------------");
                 System.out.println("id = " + rs.getString("id"));
                 System.out.println("name = " + rs.getString("name"));
                 System.out.println("--------------------");
                 label1.setText("ID - " + rs.getString("id") + " Name - " + rs.getString("name"));
-                ta.setText(ta.getText() + "ID - " + rs.getString("id") + "\nName - " + rs.getString("name") + "\n\n");
+                ta.append("ID - " + rs.getString("id") + "\nName - " + rs.getString("name") + "\n\n");
+//                ta.setText(ta.getText() + "ID - " + rs.getString("id") + "\nName - " + rs.getString("name") + "\n\n");
 
             }
+            rs.close();
             c.close();
+            frame.pack();
         } catch (Exception e){
             System.out.print(e.getMessage());
         }
-
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(layout.createSequentialGroup()
-                                                        .addComponent(ta).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED).addComponent(butt)
-                                        ))));
-
-        frame.pack();
-        frame.setVisible(true);
-
     }
+
 }
