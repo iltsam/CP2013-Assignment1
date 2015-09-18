9e3602168a56b67c9bf8abc62e0b94057777870a
Ricky Awari <ricky.awari@my.jcu.edu.au>
2015-09-18 20:01:39 +1000

    Cleaned up the GUI
    Hey guys I just made the GUI a bit neat and tidy.

--- PayrollCaseStudy/GUI2.py
+++ PayrollCaseStudy/GUI2.py
@@ -1,9 +1,10 @@
 import tkinter
 
+
 class PayrollGui:
 
-
     def __init__(self):
+
         #create main window
         self.mainWindow = tkinter.Tk()
         self.mainWindow.title("Simmar-Tech Payroll System")
@@ -13,66 +14,64 @@ class PayrollGui:
         self.mainWindow.configure(background = "white")
         #create buttons
 
-        self.homeButton = tkinter.Button(self.mainWindow, text = "Home", fg = "black", bg = "white", command = self.displayHomeWindow)
+        self.homeButton = tkinter.Button(self.mainWindow, text = "Home", fg = "black", bg = "white",width = 15, command = self.displayHomeWindow)
         self.homeButton.grid(row = 0, column = 0,columnspan = 2)
 
-        self.AddEditPayroll = tkinter.Button(self.mainWindow, text = "Add/Edit Payroll", fg = "black", bg = "white", command = self.displayPayrollWindow)
+        self.AddEditPayroll = tkinter.Button(self.mainWindow, text = "Add/Edit Payroll", fg = "black", bg = "white",width = 15, command = self.displayPayrollWindow)
         self.AddEditPayroll.grid(row = 0, column = 2, columnspan = 2)
 
-        self.EmployeeDetailsButton = tkinter.Button(self.mainWindow, text = "Employee Details", fg = "black", bg = "white", command = self.displayEmployeeDetailsWindow)
+        self.EmployeeDetailsButton = tkinter.Button(self.mainWindow, text = "Employee Details", fg = "black", bg = "white",width = 15, command = self.displayEmployeeDetailsWindow)
         self.EmployeeDetailsButton.grid(row = 0, column = 4, columnspan = 2)
 
-        self.RunPayrollButton = tkinter.Button(self.mainWindow, text = "Run Payroll", fg = "black", bg = "white")
+        self.RunPayrollButton = tkinter.Button(self.mainWindow, text = "Run Payroll", fg = "black", bg = "white", width = 15,command = self.displayRunPayrollWindow)
         self.RunPayrollButton.grid(row = 0, column = 6)
 
         #create panels
         #home Panel
         self.DisplayPanelLeft = tkinter.Label(self.mainWindow, text = "News", bg = "white")
-        self.DisplayPanelRight = tkinter.Label(self.mainWindow, bg = "white", text = "Welcome to the Simmar-Teck Payroll System.\n You can use this payroll system \n to add "
-        "new employees or edit \n existing employees."
-        "You can edit and\n monitor your pay and run the payroll here.")#, textvariable = self.message)
+
         self.DisplayPanelLeft.grid(column = 1, row = 1, columnspan = 5, rowspan = 5)
-        self.DisplayPanelRight.grid(column = 7, row = 5, columnspan = 5, rowspan = 5)
+
 
         #Payroll Add/Edit Panels
-        self.SearchButton = tkinter.Button(self.mainWindow, text = "Search", bg = "white")
-        self.DataField = tkinter.Entry(self.mainWindow, bg = "white")
-
+        self.SearchButton = tkinter.Button(self.mainWindow, text = "Search", bg = "white", width = 15)
+        self.DataField = tkinter.Entry(self.mainWindow, bg = "white", width = 40)
 
         #Add/Edit Employee
-        self.NewEmployeeButton = tkinter.Button(self.mainWindow, text = "New Employee", bg = "white")
-        self.EditEmployeeButton = tkinter.Button(self.mainWindow, text = "Edit Employee Details", bg = "white")
+        self.NewEmployeeButton = tkinter.Button(self.mainWindow, text = "New Employee", bg = "white", width = 15)
+        self.EditEmployeeButton = tkinter.Button(self.mainWindow, text = "Edit", bg = "white", width = 15)
+
+        #Run Payroll Window
+        self.runPayrollButton = tkinter.Button(self.mainWindow, text = "Run Payroll", bg = "white", width = 15)
 
         self.message = tkinter.StringVar()
         self.mainWindow.mainloop()
 
-
-
     def clearWindow(self):
-        self.DisplayPanelRight.grid_forget()
         self.DisplayPanelLeft.grid_forget()
         self.SearchButton.grid_forget()
         self.DataField.grid_forget()
         self.EditEmployeeButton.grid_forget()
         self.NewEmployeeButton.grid_forget()
+        self.runPayrollButton.grid_forget()
 
     def displayHomeWindow(self):
         self.clearWindow()
         self.DisplayPanelLeft.grid(column = 1, row = 1, columnspan = 5, rowspan = 5)
-        self.DisplayPanelRight.grid(column = 7, row = 5, columnspan = 5, rowspan = 5)
 
     def displayPayrollWindow(self):
         self.clearWindow()
-        self.SearchButton.grid(column =1, row = 1)
-        self.DataField.grid(columnspan = 3, column = 2, row=1)
+        self.SearchButton.grid(column =2, row = 3)
+        self.DataField.grid(column = 4, row = 3, columnspan = 9)
 
     def displayEmployeeDetailsWindow(self):
         self.clearWindow()
         self.EditEmployeeButton.grid(column = 5, row = 3)
         self.NewEmployeeButton.grid(column = 3, row = 3)
 
-    def displayRunPayrollWIndow(self):
+    def displayRunPayrollWindow(self):
         self.clearWindow()
+        self.runPayrollButton.grid(column = 2, row = 8)
 
 run_gui = PayrollGui()
 
