e4577aba1b63ab760984596453ebcd1b12eec1b4
Sam Marschke <samuel.marschke@my.jcu.edu.au>
2015-10-25 13:43:16 +1000

    Finished Setters

--- Payroll_Java/Payroll/src/Payroll.java
+++ Payroll_Java/Payroll/src/Payroll.java
@@ -5,12 +5,11 @@ public class Payroll {
 	private static PayrollGUI gui = new PayrollGUI();
 	public static void main(String[] args) {
 		gui.main();
-		
 		test();
 	}
 	
 	private static void test() {
-		System.out.println(gui.getFirstName());
+		gui.setGender("Female");
 	}
 
 }
