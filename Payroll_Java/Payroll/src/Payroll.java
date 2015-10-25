be6eed91624036778b347b57ba91d6a6216e00e0
Sam Marschke <samuel.marschke@my.jcu.edu.au>
2015-10-25 17:14:05 +1000

    Added mysql connectivity

--- Payroll_Java/Payroll/src/Payroll.java
+++ Payroll_Java/Payroll/src/Payroll.java
@@ -1,15 +1,23 @@
+import java.sql.*;
 
-
-
+// Public ip 123.211.106.137
 public class Payroll {
+	
 	private static PayrollGUI gui = new PayrollGUI();
-	public static void main(String[] args) {
+	private static DatabaseController db = new DatabaseController();
+	
+	public static void main(String[] args) throws Exception{
 		gui.main();
 		test();
+		db.dbTest();
+		
+
 	}
-	
+
 	private static void test() {
 		gui.setGender("Female");
 	}
+	
+	
 
 }
